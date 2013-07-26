47d96114ce37fa70f551f95d9cc26d614de4683c
cooltoast <mpatel678@gmail.com>
2013-07-26 19:56:45 -0400

    put detected_edges in their place

--- canny.py
+++ canny.py
@@ -5,7 +5,7 @@ def CannyThreshold(lowThreshold):
     detected_edges = cv2.GaussianBlur(gray,(3,3),0)
     detected_edges = cv2.Canny(detected_edges,lowThreshold,lowThreshold*ratio,apertureSize = kernel_size)
     dst = cv2.bitwise_and(img,img,mask = detected_edges)  # just add some colours to edges from original image.
-    cv2.imshow('canny demo',detected_edges)
+    cv2.imshow('canny demo',detected_edges) #replaced dst with detected_edges
 
 lowThreshold = 0
 max_lowThreshold = 100
